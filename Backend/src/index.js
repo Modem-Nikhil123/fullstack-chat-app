f9441ff4ce3edcaf7199f26e744f03918cd53548
Nikhil <nikhilm0418@gmail.com>
2025-06-03 14:48:29 +0000

    index file updated

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const express = require('express');
 const cookieParser=require('cookie-parser');
 const cors=require('cors');
@@ -11,13 +10,13 @@ const path = require('path');
 dotenv.config();
 
 const PORT=process.env.PORT;
-const __dirname=path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true,
 }));
+
 app.use('/auth',authRoutes);
 app.use('/messages',messageRoutes);
 
